cd8e0c09f7fdbaf3ac798a056d87930e88ec4a51
branthebuidler <>
2022-06-11 07:47:46 +0000

    Fix code style issues with Prettier

--- src/utils/validateAndParseAddress.ts
+++ src/utils/validateAndParseAddress.ts
@@ -1,13 +1,12 @@
-import {getChecksumAddress, validateChecksumAddress} from "starknet";
+import { getChecksumAddress, validateChecksumAddress } from 'starknet'
 
 /**
  * Validates an address and returns the parsed (checksummed) version of that address
  * @param address the unchecksummed hex address
  */
 export function validateAndParseAddress(address: string): string {
-    let checksumAddress = getChecksumAddress(address);
-    if (validateChecksumAddress(checksumAddress)) {
-      return checksumAddress;
-    }
-    else throw new Error(`${address} is not a valid address.`);
+  let checksumAddress = getChecksumAddress(address)
+  if (validateChecksumAddress(checksumAddress)) {
+    return checksumAddress
+  } else throw new Error(`${address} is not a valid address.`)
 }
